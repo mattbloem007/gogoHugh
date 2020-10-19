52674cd52a140f6be5eb6ed9b8bbd105094868e0
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2020-10-19 12:45:25 +0200

    no logo2

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ export default class Header extends Component {
       <header className={`site-header ${menu ? "active" : ""}`}>
         <div className="container">
           <div className="header-main">
-            <div className="logo">
+            {/**<div className="logo">
               <Link to="/">
                 {data.logo.file.url ? (
                   <img src={data.logo.file.url} alt="logo" />
@@ -24,7 +24,7 @@ export default class Header extends Component {
                   <span>{data.siteName}</span>
                 )}
               </Link>
-            </div>
+            </div>*/}
             <div
               className="responsive-menu"
               onClick={() => {
